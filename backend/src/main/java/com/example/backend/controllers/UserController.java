e7164f67fcb8d09808dcbc899f8be0a51abcdf37
workanhnguyen <anhnguyenvan0910@gmail.com>
2023-07-05 16:09:18 +0700

    create user dao

---
+++ backend/src/main/java/com/example/backend/controllers/UserController.java
@@ -0,0 +1,21 @@
+package com.example.backend.controllers;
+
+import com.example.backend.daos.UserDao;
+import com.example.backend.models.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserDao userDao;
+
+    @GetMapping("/user/get-all")
+    public List<User> getAllUsers() {
+        return userDao.getAllUsers();
+    }
+}
