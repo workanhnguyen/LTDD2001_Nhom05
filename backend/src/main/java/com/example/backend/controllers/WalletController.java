8e7ab5b871431f1417e98dacb34987ec3ebbfd51
workanhnguyen <anhnguyenvan0910@gmail.com>
2023-07-07 21:21:31 +0700

    init wallet dao

---
+++ backend/src/main/java/com/example/backend/controllers/WalletController.java
@@ -0,0 +1,22 @@
+package com.example.backend.controllers;
+
+import com.example.backend.daos.WalletDao;
+import com.example.backend.models.Wallet;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class WalletController {
+
+    @Autowired
+    private WalletDao walletDao;
+
+    @GetMapping("/wallet/get-all")
+    private List<Wallet> getAllWallets() {
+        return walletDao.getAllWallets();
+    }
+}
