9e94dbe922e81314aad20f6359489a137036077b
workanhnguyen <anhnguyenvan0910@gmail.com>
2023-07-10 18:17:40 +0700

    delete file not use

--- backend/src/main/java/com/example/backend/repositories/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.backend.repositories;
-
-import com.example.backend.models.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Integer> {
-}
