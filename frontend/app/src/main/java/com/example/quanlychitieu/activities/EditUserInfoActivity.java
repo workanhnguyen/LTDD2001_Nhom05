fb65330381dc6c3a7bd15d1775d44d4a2ba42f7a
workanhnguyen <anhnguyenvan0910@gmail.com>
2023-07-15 17:10:08 +0700

    init edit user info UI

---
+++ frontend/app/src/main/java/com/example/quanlychitieu/activities/EditUserInfoActivity.java
@@ -0,0 +1,35 @@
+package com.example.quanlychitieu.activities;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.example.quanlychitieu.R;
+
+public class EditUserInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_user_info);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setTitle("");
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
