245dc4233166f545f5621e2eba64e42e990ab7b0
workanhnguyen <anhnguyenvan0910@gmail.com>
2023-07-15 18:27:21 +0700

    init statistic filter ui

--- frontend/app/src/main/java/com/example/quanlychitieu/activities/StatisticFilterActivity.java
+++ frontend/app/src/main/java/com/example/quanlychitieu/activities/StatisticFilterActivity.java
@@ -2,13 +2,24 @@ package com.example.quanlychitieu.activities;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 import android.view.MenuItem;
 
 import com.example.quanlychitieu.R;
+import com.example.quanlychitieu.adapters.ViewPagerAdapter;
+import com.example.quanlychitieu.fragment.CustomTimeSatisticFragment;
+import com.example.quanlychitieu.fragment.DayStatisticFragment;
+import com.example.quanlychitieu.fragment.MonthStatisticFragment;
+import com.google.android.material.tabs.TabLayout;
 
 public class StatisticFilterActivity extends AppCompatActivity {
+    TabLayout statisticTabLayout;
+    ViewPager statisticViewPager;
+
+    ViewPagerAdapter viewPagerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +32,25 @@ public class StatisticFilterActivity extends AppCompatActivity {
             actionBar.setTitle(R.string.data_setting);
             actionBar.setElevation(0);
         }
+
+        initializeElement();
+        setTabLayoutAdapter();
+    }
+
+    private void setTabLayoutAdapter() {
+        statisticTabLayout.setupWithViewPager(statisticViewPager);
+
+        viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager(), FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        viewPagerAdapter.addFragment(new DayStatisticFragment(), getString(R.string.date));
+        viewPagerAdapter.addFragment(new MonthStatisticFragment(), getString(R.string.month));
+        viewPagerAdapter.addFragment(new CustomTimeSatisticFragment(), getString(R.string.custom_time));
+
+        statisticViewPager.setAdapter(viewPagerAdapter);
+    }
+
+    private void initializeElement() {
+        statisticTabLayout = findViewById(R.id.statisticTabLayout);
+        statisticViewPager = findViewById(R.id.statisticViewPager);
     }
 
     @Override
---
+++ frontend/app/src/main/java/com/example/quanlychitieu/fragment/CustomTimeSatisticFragment.java
@@ -0,0 +1,19 @@
+package com.example.quanlychitieu.fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.quanlychitieu.R;
+public class CustomTimeSatisticFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_custom_time_satistic, container, false);
+    }
+}
