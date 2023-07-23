ccabb7987afe450c2c1136fb0c052a5eed25cfa7
dev_quynh <nhuquynh06032002@gmail.com>
2023-07-23 18:34:37 +0700

    update setting multiple languages

--- frontend/app/src/main/java/com/example/quanlychitieu/activities/WelcomeActivity.java
+++ frontend/app/src/main/java/com/example/quanlychitieu/activities/WelcomeActivity.java
@@ -1,6 +1,10 @@
 package com.example.quanlychitieu.activities;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
@@ -17,6 +21,7 @@ import com.example.quanlychitieu.adapters.SpinnerLanguageAdapter;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 public class WelcomeActivity extends AppCompatActivity {
     private long backPressedTime;
@@ -41,11 +46,43 @@ public class WelcomeActivity extends AppCompatActivity {
         handleSwitchToRegisterActivity();
         handleSwitchToLoginActivity();
 
+
+
+        // Set the locale based on the user's preferred language
+
+
+        SharedPreferences sharedPreferences = getSharedPreferences("MyPrefs", MODE_PRIVATE);
+        String selectedLanguage = sharedPreferences.getString("language", "vi");
+
+        setLocale(selectedLanguage);
+
+        if (selectedLanguage.equals("vi")) {
+            spinnerLanguage.setSelection(0);
+        } else if (selectedLanguage.equals("en")) {
+            spinnerLanguage.setSelection(1);
+        }
+
+        SpinnerLanguageAdapter adapter = new SpinnerLanguageAdapter(WelcomeActivity.this, languages);
+        spinnerLanguage.setAdapter(adapter);
+
         spinnerLanguage.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 String item = parent.getItemAtPosition(position).toString();
                 Toast.makeText(WelcomeActivity.this, item, Toast.LENGTH_SHORT).show();
+                String selectedLanguage;
+                if (position == 0) {
+                    selectedLanguage = "vi"; // Vietnamese
+                } else {
+                    selectedLanguage = "en"; // English
+                }
+
+                String currentLanguage = getSelectedLanguage();
+                if (!selectedLanguage.equals(currentLanguage)) {
+                    setLocale(selectedLanguage);
+                    recreate();
+                }
+
             }
 
             @Override
@@ -53,8 +90,6 @@ public class WelcomeActivity extends AppCompatActivity {
 
             }
         });
-        SpinnerLanguageAdapter adapter = new SpinnerLanguageAdapter(WelcomeActivity.this, languages);
-        spinnerLanguage.setAdapter(adapter);
     }
 
     private void handleSwitchToLoginActivity() {
@@ -102,5 +137,38 @@ public class WelcomeActivity extends AppCompatActivity {
             Toast.makeText(this, getString(R.string.press_again_to_exit), Toast.LENGTH_SHORT).show();
         }
         backPressedTime = System.currentTimeMillis();
+    }
+
+//    public void setLocale(Context context, String lang) {
+//        Locale locale = new Locale(lang);
+//        Locale.setDefault(locale);
+//        Resources resources = getResources();
+//        Configuration config = new Configuration();
+//        config.setLocale(locale);
+//        resources.updateConfiguration(config, resources.getDisplayMetrics());
+//
+//        // Save the selected language in SharedPreferences
+//        SharedPreferences sharedPreferences = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE);
+//        SharedPreferences.Editor editor = sharedPreferences.edit();
+//        editor.putString("language", lang);
+//        editor.apply();
+//    }
+    private void setLocale(String lang) {
+        Locale locale = new Locale(lang);
+        Locale.setDefault(locale);
+        Resources resources = getResources();
+        Configuration config = new Configuration();
+        config.setLocale(locale);
+        resources.updateConfiguration(config, resources.getDisplayMetrics());
+
+        SharedPreferences sharedPreferences = getSharedPreferences("MyPrefs", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("language", lang);
+        editor.apply();
+}
+
+    private String getSelectedLanguage() {
+        SharedPreferences sharedPreferences = getSharedPreferences("MyPrefs", MODE_PRIVATE);
+        return sharedPreferences.getString("language", "vi");
     }
 }
