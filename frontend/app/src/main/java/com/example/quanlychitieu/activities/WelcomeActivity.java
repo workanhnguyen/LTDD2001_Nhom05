dddbc6fb93fc53d8a93b937c189f95fd335b285a
workanhnguyen <anhnguyenvan0910@gmail.com>
2023-07-24 10:52:51 +0700

    handle change activity if logged in

--- frontend/app/src/main/java/com/example/quanlychitieu/activities/WelcomeActivity.java
+++ frontend/app/src/main/java/com/example/quanlychitieu/activities/WelcomeActivity.java
@@ -1,6 +1,8 @@
 package com.example.quanlychitieu.activities;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
@@ -12,8 +14,10 @@ import android.widget.Toast;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.quanlychitieu.MainActivity;
 import com.example.quanlychitieu.R;
 import com.example.quanlychitieu.adapters.SpinnerLanguageAdapter;
+import com.example.quanlychitieu.configs.LoggingUserInfo;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -27,34 +31,45 @@ public class WelcomeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_welcome);
+        if (isUserLoggedIn()) {
+            // Nếu đã đăng nhập, chuyển hướng đến giao diện chính
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+            finish(); // Đóng activity hiện tại để người dùng không thể quay lại giao diện đăng nhập
+        } else {
+            setContentView(R.layout.activity_welcome);
+            ActionBar actionBar = getSupportActionBar();
+            if (actionBar != null) {
+                actionBar.hide();
+            }
 
-        ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null) {
-            actionBar.hide();
+            languages.add(getString(R.string.language_vn));
+            languages.add(getString(R.string.language_en));
+
+            initializeElement();
+            handleSwitchToRegisterActivity();
+            handleSwitchToLoginActivity();
+
+            spinnerLanguage.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                @Override
+                public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                    String item = parent.getItemAtPosition(position).toString();
+                    Toast.makeText(WelcomeActivity.this, item, Toast.LENGTH_SHORT).show();
+                }
+
+                @Override
+                public void onNothingSelected(AdapterView<?> parent) {
+
+                }
+            });
+            SpinnerLanguageAdapter adapter = new SpinnerLanguageAdapter(WelcomeActivity.this, languages);
+            spinnerLanguage.setAdapter(adapter);
         }
+    }
 
-        languages.add(getString(R.string.language_vn));
-        languages.add(getString(R.string.language_en));
-
-        initializeElement();
-        handleSwitchToRegisterActivity();
-        handleSwitchToLoginActivity();
-
-        spinnerLanguage.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                String item = parent.getItemAtPosition(position).toString();
-                Toast.makeText(WelcomeActivity.this, item, Toast.LENGTH_SHORT).show();
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
-            }
-        });
-        SpinnerLanguageAdapter adapter = new SpinnerLanguageAdapter(WelcomeActivity.this, languages);
-        spinnerLanguage.setAdapter(adapter);
+    private boolean isUserLoggedIn() {
+        SharedPreferences sharedPreferences = getSharedPreferences("LoggingUser", Context.MODE_PRIVATE);
+        return sharedPreferences.getInt("userId", -1) != -1;
     }
 
     private void handleSwitchToLoginActivity() {
