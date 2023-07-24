26af9afc30c20c6b17a6b589c37651da59b62c06
workanhnguyen <anhnguyenvan0910@gmail.com>
2023-07-24 21:26:55 +0700

    add pass data interface

---
+++ frontend/app/src/main/java/com/example/quanlychitieu/utils/PassData.java
@@ -0,0 +1,7 @@
+package com.example.quanlychitieu.utils;
+
+import com.example.quanlychitieu.models.CategoryType;
+
+public interface PassData {
+    void passCategoryTypeData(CategoryType categoryType);
+}
