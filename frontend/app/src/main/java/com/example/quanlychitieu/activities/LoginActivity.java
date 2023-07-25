f47d9ce439c2d88b66eff1eb71a42a760159c8fa
workanhnguyen <anhnguyenvan0910@gmail.com>
2023-07-25 18:01:11 +0700

    handle login

--- frontend/app/src/main/java/com/example/quanlychitieu/activities/LoginActivity.java
+++ frontend/app/src/main/java/com/example/quanlychitieu/activities/LoginActivity.java
@@ -3,22 +3,31 @@ package com.example.quanlychitieu.activities;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.res.ColorStateList;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import com.example.quanlychitieu.MainActivity;
 import com.example.quanlychitieu.R;
 import com.example.quanlychitieu.configs.LoggingUserInfo;
+import com.example.quanlychitieu.models.User;
+import com.example.quanlychitieu.presenters.LoginPresenter;
+import com.example.quanlychitieu.views.LoginView;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements LoginView {
     private long backPressedTime;
-    TextView switchToRegister;
+    TextView switchToRegister, loginAlert, loginUsername;
+    EditText loginPassword;
     Button btnLogin;
+    LoginPresenter loginPresenter;
+    SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +39,9 @@ public class LoginActivity extends AppCompatActivity {
             actionBar.hide();
         }
 
+        loginPresenter = new LoginPresenter(this);
+        sharedPreferences = getSharedPreferences("loggingUser", Context.MODE_PRIVATE);
+
         initializeElement();
         handleSwitchToRegister();
         handleLogin();
@@ -39,15 +51,29 @@ public class LoginActivity extends AppCompatActivity {
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                SharedPreferences sharedPreferences = getSharedPreferences("LoggingUser", Context.MODE_PRIVATE);
-                SharedPreferences.Editor editor = sharedPreferences.edit();
-                editor.putInt("userId", LoggingUserInfo.user.getId());
-                editor.apply();
+                loginAlert.setVisibility(View.GONE);
 
-                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                startActivity(intent);
+                String username = loginUsername.getText().toString();
+                String password = loginPassword.getText().toString();
+                String alertInfo = validateLoginInfo(username, password);
+
+                if (alertInfo.trim().isEmpty()) {
+                    btnLogin.setEnabled(false);
+                    btnLogin.setBackgroundTintList(ColorStateList.valueOf(ContextCompat.getColor(LoginActivity.this, R.color.light_grey)));
+                    btnLogin.setText(getString(R.string.logging));
+                    loginPresenter.login(new User(username, password));
+                } else {
+                    loginAlert.setVisibility(View.VISIBLE);
+                    loginAlert.setText(alertInfo);
+                }
             }
         });
+    }
+
+    private String validateLoginInfo(String username, String password) {
+        if (username.trim().isEmpty() || password.trim().isEmpty())
+            return getString(R.string.not_empty_field);
+        return "";
     }
 
     private void handleSwitchToRegister() {
@@ -62,8 +88,42 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     private void initializeElement() {
+        loginAlert = findViewById(R.id.loginAlert);
+        loginUsername = findViewById(R.id.loginUsername);
+        loginPassword = findViewById(R.id.loginPassword);
         switchToRegister = findViewById(R.id.switchToRegister);
         btnLogin = findViewById(R.id.btnLogin);
+    }
+
+    @Override
+    public void showLoginInfo(User user) {
+        btnLogin.setEnabled(true);
+        btnLogin.setBackgroundTintList(ColorStateList.valueOf(ContextCompat.getColor(LoginActivity.this, R.color.primary)));
+        btnLogin.setText(getString(R.string.login));
+        if (user != null) {
+            SharedPreferences sharedPreferences = getSharedPreferences("loggingUser", Context.MODE_PRIVATE);
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putInt("id", user.getId());
+            editor.putString("firstName", user.getFirstname());
+            editor.putString("lastName", user.getLastname());
+            editor.putString("username", user.getUsername());
+            editor.putString("career", user.getCareer());
+            editor.putString("email", user.getEmail());
+            editor.putString("imageLink", user.getImageLink());
+            editor.putBoolean("gender", user.isGender());
+            editor.apply();
+
+            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+            startActivity(intent);
+        } else {
+            loginAlert.setVisibility(View.VISIBLE);
+            loginAlert.setText(getString(R.string.username_or_password_not_correct));
+        }
+    }
+
+    @Override
+    public void showLoginError() {
+        loginAlert.setText(getString(R.string.unknown_error));
     }
 
 //    @Override
