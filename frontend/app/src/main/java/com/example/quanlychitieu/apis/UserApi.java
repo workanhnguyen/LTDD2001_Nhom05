f407606baaa88030e5bfaf3f2a12df5d1b578c13
workanhnguyen <anhnguyenvan0910@gmail.com>
2023-07-25 18:00:12 +0700

    handle login

---
+++ frontend/app/src/main/java/com/example/quanlychitieu/apis/UserApi.java
@@ -0,0 +1,12 @@
+package com.example.quanlychitieu.apis;
+
+import com.example.quanlychitieu.models.User;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.POST;
+
+public interface UserApi {
+    @POST("/users/login")
+    Call<User> login(@Body User requestUser);
+}
