53d126faa3d7edfc67664101d989b22425ce2d99
workanhnguyen <anhnguyenvan0910@gmail.com>
2023-07-26 22:00:33 +0700

    add new constant

--- frontend/app/src/main/java/com/example/quanlychitieu/utils/CustomConstant.java
+++ frontend/app/src/main/java/com/example/quanlychitieu/utils/CustomConstant.java
@@ -1,13 +1,11 @@
 package com.example.quanlychitieu.utils;
 
 public class CustomConstant {
-    public static final String FILTER_STATISTIC_YESTERDAY= "YESTERDAY";
-    public static final String FILTER_STATISTIC_TODAY= "TODAY";
-    public static final String FILTER_STATISTIC_OTHER_DAY= "OTHER_DAY";
-
     public static final String FILTER_STATISTIC_LAST_MONTH= "LAST_MONTH";
     public static final String FILTER_STATISTIC_THIS_MONTH= "THIS_MONTH";
     public static final String FILTER_STATISTIC_OTHER_MONTH= "OTHER_MONTH";
     public static final String CATEGORY_EXPENSE = "EXPENSE";
     public static final String CATEGORY_INCOME = "INCOME";
+    public static final String ROLE_USER = "ROLE_USER";
+    public static final String ROLE_ADMIN = "ROLE_ADMIN";
 }
