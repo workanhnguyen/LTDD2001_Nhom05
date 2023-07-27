151e6d4e578d95e8229a1fe5c63a0f9b51d2cb32
workanhnguyen <anhnguyenvan0910@gmail.com>
2023-07-27 09:11:52 +0700

    add new constants

--- frontend/app/src/main/java/com/example/quanlychitieu/utils/CommonUtil.java
+++ frontend/app/src/main/java/com/example/quanlychitieu/utils/CommonUtil.java
@@ -7,4 +7,17 @@ public class CommonUtil {
     public static String getMoneyFormat(long money) {
         return NumberFormat.getCurrencyInstance(new Locale("vi", "VN")).format(money);
     }
+
+    public static String[] getFirstAndLastName(String fullName) {
+        String[] nameParts = fullName.trim().split("\\s+", 2); // Split by whitespace, limit to 2 parts
+
+        String lastName = nameParts[0];
+        String firstName = "";
+
+        if (nameParts.length > 1) {
+            firstName = nameParts[1];
+        }
+
+        return new String[]{lastName, firstName};
+    }
 }
